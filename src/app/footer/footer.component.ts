feeb6bd7c01c88ebda445310bbe1a8ddd7bd1166
Hans-Christiaan Braun <hc.braun.93@gmail.com>
2018-02-04 22:55:59 +0100

    Errors are now hidden when category edit dialog is hidden.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -18,18 +18,32 @@ export class FooterComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  /**
+   * Hides the login dialog again and removes any error
+   * messages.
+   */
+  public hideLoginDialog() {
+    this.error = null;
+    this.loginModal.hide();
+  }
   
+  /**
+   * Try to login using the given username and password.
+   * @param {string} username the username to use.
+   * @param {string} password the password to use.
+   */
   public login(username: string, password: string) {  
 
     this.authenticate.login(username, password)
       .subscribe((user) => {
-        console.log(user, ' logged in!');
+        console.log(`${user.username} logged in!`);
         this.error = null;
         this.loginModal.hide();
       },
       (error) => {
         switch(error.status) {
-          case 401: this.error = 'Wrong user name or password.'; break ;
+          case 401: this.error = 'Wrong username or password.'; break ;
           default: this.error = `${error.status} - ${error.statusText}`;
         }
       });
